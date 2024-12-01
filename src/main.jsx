c03f840d1a2d65e1edd757058492442aa9b6802a
Angelapeters1601 <nwattaha@gmail.com>
2024-12-01 03:50:14 +0100

    Connected react and redux using react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-console.log(store.getState());
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
-
-// The exported actions will be dispatched to react where needed.
